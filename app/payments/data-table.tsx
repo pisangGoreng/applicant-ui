b6f0994f4346b95ea95d5ca0532e33e3625b7c58
pisangGoreng <7ofpentacles@gmail.com>
2024-10-25 22:33:22 +0700

    add row clicked handler

--- app/payments/data-table.tsx
+++ app/payments/data-table.tsx
@@ -11,6 +11,13 @@ import {
   SortingState,
   useReactTable,
 } from "@tanstack/react-table";
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectContent,
+  SelectValue,
+} from "@/components/ui/select";
 
 import {
   Table,
@@ -26,12 +33,26 @@ import { Input } from "@/components/ui/input";
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
+  applicantStatus: any;
+  applicantRole: any;
 }
+
+const initialData = [
+  { id: 1, name: "Alice", role: "Admin", status: "Active" },
+  { id: 2, name: "Bob", role: "User", status: "Inactive" },
+  { id: 3, name: "Charlie", role: "Admin", status: "Active" },
+  { id: 4, name: "David", role: "User", status: "Inactive" },
+];
 
 export function DataTable<TData, TValue>({
   columns,
   data,
+  applicantStatus,
+  applicantRole,
 }: DataTableProps<TData, TValue>) {
+  const [dataSelect, setDataSelect] = useState(initialData); // Table data state
+  const [filter, setFilter] = useState(""); // Dropdown filter state
+
   const [sorting, setSorting] = useState<SortingState>([]);
 
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
@@ -49,6 +70,22 @@ export function DataTable<TData, TValue>({
     },
   });
 
+  const handleStatusFiltering = (value: string) => {
+    table
+      .getColumn("applicantStatus")
+      ?.setFilterValue(value === "All" ? "" : value);
+  };
+
+  const handleRoleFiltering = (value: string) => {
+    table
+      .getColumn("applicantRole")
+      ?.setFilterValue(value === "All" ? "" : value);
+  };
+
+  const handleRowClick = (applicantDetails: any) => {
+    console.log("applicantDetails ", applicantDetails);
+  };
+
   return (
     <div className="rounded-md border">
       <div className="flex items-center py-4">
@@ -56,11 +93,42 @@ export function DataTable<TData, TValue>({
           placeholder="Search name..."
           value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
           onChange={(event) => {
-            console.log("cek ", event.target.value);
             table.getColumn("name")?.setFilterValue(event.target.value);
           }}
           className="max-w-sm"
         />
+
+        <Select onValueChange={(value) => handleStatusFiltering(value)}>
+          <SelectTrigger className="w-48">
+            <SelectValue placeholder="Select Status" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value={"All"}>All</SelectItem>
+            {applicantStatus.map(
+              ({ id, description }: { id: string; description: string }) => (
+                <SelectItem key={id} value={description}>
+                  {description}
+                </SelectItem>
+              )
+            )}
+          </SelectContent>
+        </Select>
+
+        <Select onValueChange={(value) => handleRoleFiltering(value)}>
+          <SelectTrigger className="w-48">
+            <SelectValue placeholder="Select Role" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value={"All"}>All</SelectItem>
+            {applicantRole.map(
+              ({ id, description }: { id: string; description: string }) => (
+                <SelectItem key={id} value={description}>
+                  {description}
+                </SelectItem>
+              )
+            )}
+          </SelectContent>
+        </Select>
       </div>
 
       <div className="flex items-center justify-end space-x-2 py-4">
@@ -107,6 +175,7 @@ export function DataTable<TData, TValue>({
               <TableRow
                 key={row.id}
                 data-state={row.getIsSelected() && "selected"}
+                onClick={() => handleRowClick(row.original)}
               >
                 {row.getVisibleCells().map((cell) => (
                   <TableCell key={cell.id}>
--- app/payments/page.tsx
+++ app/payments/page.tsx
@@ -2,16 +2,29 @@ import { columns, Applicants } from "./columns";
 import { DataTable } from "./data-table";
 
 export default async function DemoPage() {
-  const res = await fetch(`http://localhost:3001/applicants`, {
-    method: "GET",
-  });
-
+  const res = await fetch(`http://localhost:3001/applicants`);
   const parsedRes = await res.json();
-  // console.log("🚀 ~ parsedRes:", parsedRes.data.data);
+
+  const applicantStatus = await fetch(
+    `http://localhost:3001/applicants-status`
+  );
+  const parsedApplicantStatus = await applicantStatus.json();
+
+  const applicantRole = await fetch(`http://localhost:3001/applicants-role`);
+  const parsedApplicantRole = await applicantRole.json();
 
   return (
-    <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={parsedRes.data.data} />
+    <div className="container mx-auto py-10 grid grid-cols-3 gap-4">
+      <div className="col-span-2 shadow-lg p-4">
+        <DataTable
+          columns={columns}
+          data={parsedRes.data.data}
+          applicantStatus={parsedApplicantStatus.data}
+          applicantRole={parsedApplicantRole.data}
+        />
+      </div>
+
+      <div className="shadow-lg p-4">kucing</div>
     </div>
   );
 }
